9f33356ba8f512f35df86b900c5141cf8b344fe9
Pranav Kumar <pranav.05a@gmail.com>
2023-04-25 20:34:54 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <NavBar/>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome To SS ACADEMY"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    NavBar
+  }
+})
+</script>
